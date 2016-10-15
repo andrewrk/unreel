c9148216fa15388f9fd0d7bf0cc0a4a6e67ed91d
Damish Shah <damishshah@Damishs-MacBook-Pro-2.local>
2016-10-15 17:35:55 -0400

    added add button and video list view

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,14 +6,31 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    filePaths = new QList<QString>();
+    player = new QMediaPlayer();
 }
 
 MainWindow::~MainWindow()
 {
+    delete filePaths;
     delete ui;
 }
 
 void MainWindow::on_actionQuit_triggered()
 {
     QApplication::quit();
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    //TODO: Filter only video files
+    QString fileName = QFileDialog::getOpenFileName(this,
+        "Open Video", QString(), "Video Files (*)");
+    filePaths->append(fileName);
+    ui->videoListView->addItem(QFileInfo(fileName).baseName());
+}
+
+void MainWindow::on_videoListView_itemClicked(QListWidgetItem *item)
+{
+
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QMediaPlayer>
+#include <QFileInfo>
+#include <QListWidgetItem>
 
 namespace Ui {
 class MainWindow;
@@ -18,8 +22,14 @@ public:
 private slots:
     void on_actionQuit_triggered();
 
+    void on_pushButton_clicked();
+
+    void on_videoListView_itemClicked(QListWidgetItem *item);
+
 private:
     Ui::MainWindow *ui;
+    QList<QString> *filePaths;
+    QMediaPlayer *player;
 };
 
 #endif // MAINWINDOW_H
